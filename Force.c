55b1f06b407cf1643e891f661bb3ad128e196e92
sabbir rahman <sr492033@gmail.com>
2026-09-16 01:33:08 +0600

    Add Force calculation program in Force.c

---
+++ Force.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+   float m,a,F;
+    printf("Enter the value of m(mass):");
+    scanf("%f",&m);
+    printf("Enter the value of a(Acceleration):");
+    scanf("%f",&a);
+    F=m*a;
+    printf("Force value=%.2f",F);
+}
+    
