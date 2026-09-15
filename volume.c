a7832d7b48ec312ad23ea8a0f71f5773eedbbf3c
sabbir rahman <sr492033@gmail.com>
2026-09-16 00:59:43 +0600

    Add volume calculation program in C

---
+++ volume.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+   float length,width,height,volume;
+    printf("Enter the value of length:");
+     scanf("%f",&length);
+    printf("Enter the value of width:");
+     scanf("%f",&width);
+    printf("Enter the value of height:");
+    scanf("%f",&height);
+   // scanf("%f",&length);
+   // scanf("%f",&width);
+   // scanf("%f",&height);
+    volume=length*width*height;
+    printf("volume value=%.3f",volume);
+}
+    
